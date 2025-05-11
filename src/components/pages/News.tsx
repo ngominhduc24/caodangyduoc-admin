287588d48ce63591881ca2c3cf96e54dfec71d81
hoshikira18 <khuyen.dev183@gmail.com>
2025-05-11 21:43:20 +0700

    feat: clean code

--- src/components/pages/News.tsx
+++ src/components/pages/News.tsx
@@ -5,8 +5,6 @@ import { Link } from "react-router-dom"
 import { useState } from "react"
 import { Badge } from "../ui/badge"
 import { Input } from "../ui/input"
-import { Label } from "../ui/label"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select"
 import { useCategories } from "@/hooks/useCategories"
 
 const limit = 10
